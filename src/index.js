06aff3e588b5249ac87befa730f9751a49fb25de
Benben Bagus P <benben4567@gmail.com>
2022-07-24 01:49:19 +0700

    ui improvement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 require('dotenv').config()
-
+const figlet = require('figlet')
+const chalk = require('chalk')
 const express = require('express')
 const router = require('./routes')
 const app = express()
 const port = process.env.PORT;
 
+const config = {
+    font: 'doom',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 90,
+}
+
+
+
 app.use("/", router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.clear();
+    console.log(chalk.green(figlet.textSync('RFID Server', config)))
+    console.log(chalk.bold.yellow(`                 http://localhost:${port}`))
+    console.log(chalk.yellow('                    Scanner: ', chalk.green('READY')));
+    console.log('==========================================================')
+    console.log('')
+});
 
--- src/services/rfidService.js
+++ src/services/rfidService.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const chalk = require('chalk')
 const { SerialPort } = require('serialport')
 const { ByteLengthParser } = require('@serialport/parser-byte-length')
 
@@ -10,10 +11,10 @@ const port = new SerialPort({
 
 const singleScan = async () => {
     // port.open(function (err) {
-        // if (err) {
-            // return console.log('Error opening port: ', err.message)
-        // }
-        // console.log('Serial port open')
+    // if (err) {
+    // return console.log('Error opening port: ', err.message)
+    // }
+    // console.log('Serial port open')
     // })
 
     const parser = port.pipe(new ByteLengthParser({ length: 16 }))
@@ -25,13 +26,13 @@ const singleScan = async () => {
 
             if (hex != prev) {
                 // port.close(function (err) {
-                    // if (err) {
-                        // console.log('Error closing port: ', err.message)
-                    // }
-                    // console.log('Serial port closed')
+                // if (err) {
+                // console.log('Error closing port: ', err.message)
+                // }
+                // console.log('Serial port closed')
                 // })
                 prev = hex
-				console.log("DATA : ",hex);
+                console.log(chalk.green("[DATA]", chalk.cyan(hex)));
                 resolve(hex)
             }
         });
