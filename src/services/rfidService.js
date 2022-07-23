fa964635dee5d3d78b66a55b98a57e359e3349a6
812 BUGIS <benben4567@gmail.com>
2022-07-23 12:18:43 +0700

    fix

--- src/services/rfidService.js
+++ src/services/rfidService.js
@@ -5,18 +5,18 @@ const { ByteLengthParser } = require('@serialport/parser-byte-length')
 const port = new SerialPort({
     path: process.env.COM_PORT,
     baudRate: 57600,
-    autoOpen: false,
+    // autoOpen: false,
 })
 
 const singleScan = async () => {
-    port.open(function (err) {
-        if (err) {
-            return console.log('Error opening port: ', err.message)
-        }
-        console.log('Serial port open')
-    })
+    // port.open(function (err) {
+        // if (err) {
+            // return console.log('Error opening port: ', err.message)
+        // }
+        // console.log('Serial port open')
+    // })
 
-    const parser = port.pipe(new ByteLengthParser({ length: 8 }))
+    const parser = port.pipe(new ByteLengthParser({ length: 16 }))
 
     let prev = "";
     return new Promise((resolve, reject) => {
@@ -24,13 +24,14 @@ const singleScan = async () => {
             let hex = buff.toString('hex')
 
             if (hex != prev) {
-                port.close(function (err) {
-                    if (err) {
-                        console.log('Error closing port: ', err.message)
-                    }
-                    console.log('Serial port closed')
-                })
+                // port.close(function (err) {
+                    // if (err) {
+                        // console.log('Error closing port: ', err.message)
+                    // }
+                    // console.log('Serial port closed')
+                // })
                 prev = hex
+				console.log("DATA : ",hex);
                 resolve(hex)
             }
         });
